b2584225a1c206854f89c33ec471929aa97ea2a4
Indigo Orton <indigo@loop-hq.com>
2017-07-06 16:44:13 +1000

    Add check for same version number in build as in package.json

--- loop-deploy.sh
+++ loop-deploy.sh
@@ -6,8 +6,14 @@
 
 set -e
 
-MASTER=`"$(dirname $0)/loop-assert-master.sh" ${1}`
+BASE=$(dirname $0)
+MASTER=`"${BASE}/loop-assert-master.sh" ${1}`
 
+if [ -d "${BASE}/build" ]; then
+  if [ "`npm view ${BASE}/build version`" == "`npm view ${BASE} version`" ]; then
+    echo "Careful, you're deploying with the same version number as the version in /build"
+  fi
+fi
 npm run build
 cd build
 ${MASTER}/tools/javascript/upload-package-artifact.sh .
